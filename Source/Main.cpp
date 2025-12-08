27190a607049e831a309f10eac60500d70d70318
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-12-08 13:37:36 +0100

    Initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,35 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "../Header/Util.h"
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "RollerCoaster", NULL, NULL);
+    if (window == NULL) return endProgram("Failed to create window.");
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) return endProgram("Failed to initialize GLEW.");
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glClearColor(0.2f, 0.8f, 0.6f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
