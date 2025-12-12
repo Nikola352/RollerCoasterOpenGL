f96626663f579fb467aa227c7de6016ec7f27183
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-12-12 02:34:07 +0100

    Change track to sine wave

--- Source/Game/Track.cpp
+++ Source/Game/Track.cpp
@@ -7,19 +7,21 @@ float getTrackHeightAt(float x) {
     if (x < -0.5f) {
         return -0.5f;
     }
-    return -0.5f * cbrt(cos(12 * x + 6));
+    // return -0.5f * cbrt(cos(12 * x + 6));
+    return -0.5f * cos(12 * x + 6);
 }
 
 float getTrackSlopeAt(float x) {
     if (x < -0.5f) {
         return 0;
     }
-    float cosVal = cos(12 * x + 6);
+    // float cosVal = cos(12 * x + 6);
     float sinVal = sin(12 * x + 6);
+    return 6 * sinVal;
 
-    if (fabs(cosVal) < 1e-6) {
-        return (sinVal > 0) ? 10000.0f : -10000.0f;
-    }
+    // if (fabs(cosVal) < 1e-6) {
+    //     return (sinVal > 0) ? 10000.0f : -10000.0f;
+    // }
 
-    return 2.0f * sinVal * pow(cbrt(cosVal), -2.0f);
+    // return 2.0f * sinVal * pow(cbrt(cosVal), -2.0f);
 }
